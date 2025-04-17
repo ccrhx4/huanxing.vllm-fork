dc18ea8f63588f9684a391666dd66ebfbb163e52
huanxing <huanxing.shen@intel.com>
2025-04-17 06:45:59 +0000

    add testcase for deepseek

--- examples/offline_inference/basic.py
+++ examples/offline_inference/basic.py
@@ -5,15 +5,25 @@ from vllm import LLM, SamplingParams
 # Sample prompts.
 prompts = [
     "Hello, my name is",
-    "The president of the United States is",
-    "The capital of France is",
-    "The future of AI is",
 ]
 # Create a sampling params object.
-sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+sampling_params = SamplingParams(temperature=1.0, repetition_penalty=0.9)
 
 # Create an LLM.
-llm = LLM(model="facebook/opt-125m")
+llm = LLM(model="/models/static_deepseek",
+        enforce_eager=False,
+        hf_overrides={
+            "num_hidden_layers": 4,
+        },
+        dtype="bfloat16",
+        tensor_parallel_size=8, 
+        max_model_len=8192,
+        max_num_seqs=4,
+        max_num_batched_tokens=8192,
+        trust_remote_code=True,
+        kv_cache_dtype="fp8_inc", 
+        gpu_memory_utilization=0.9, 
+        distributed_executor_backend="ray")
 # Generate texts from the prompts. The output is a list of RequestOutput objects
 # that contain the prompt, generated text, and other information.
 outputs = llm.generate(prompts, sampling_params)

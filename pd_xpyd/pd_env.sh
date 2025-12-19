077f1f4ea406458a97de4721c34210f7aa6a62f3
huanxing <huanxing.shen@intel.com>
2025-12-19 11:50:05 +0800

    add comment about how to set sync mode

--- pd_xpyd/pd_env.sh
+++ pd_xpyd/pd_env.sh
@@ -48,7 +48,8 @@ DEBUG_LOG=1
 DEBUG_GRAPH=0
 DEBUG_PROFILE=0
 INC_FP8=0
-BENCHMARK_MODE=0
+# When using benchmark mode (not benchmark_decode) in proxy server, be sure to set BENCHMARK_MODE=1
+BENCHMARK_MODE=0 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CONFIG END ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 if [ "$BENCHMARK_MODE" -eq 1 ]; then

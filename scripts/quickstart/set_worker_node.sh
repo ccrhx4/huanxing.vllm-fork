2240e641c0459edaa3623fc5e9a80da02326ac41
huanxing <huanxing.shen@intel.com>
2026-01-15 03:16:29 +0000

    update tp16 setup

--- scripts/quickstart/set_worker_node.sh
+++ scripts/quickstart/set_worker_node.sh
@@ -21,13 +21,30 @@ source "$BASH_DIR"/utils.sh
 
 # INC FP8 quantization
 export INC_MEASUREMENT_DUMP_PATH_PREFIX=$(realpath "$BASH_DIR/../..")
-export QUANT_CONFIG=$(realpath "$BASH_DIR/../quant_configs/inc_quant_per_channel_bf16kv.json")
+export QUANT_CONFIG=$(realpath "$BASH_DIR/../quant_configs/inc_quant_fp8kv_pts_scalar_fp8_mla.json")
 if [ -n "$QUANT_CONFIG" ]; then
     export VLLM_REQUANT_FP8_INC=1
     export VLLM_ENABLE_RUNTIME_DEQUANT=1
     export VLLM_HPU_MARK_SCALES_AS_CONST=false
     export VLLM_MOE_N_SLICE=1
     export INC_FORCE_NAIVE_SCALING=1
+
+    # Set RUNTIME_SCALE_PATCHING when scale_format equals "scalar" in quant config
+    export RUNTIME_SCALE_PATCHING=1
+
+    # Enable QKV slicing for long prompt
+    export INC_APPLY_OOT_PATCH="true"
+    export PT_HPU_SDPA_QKV_SLICE_MODE_FWD=1
+    export VLLM_HPU_FSDPA_SLICE_SEQ_LEN_THLD=16384
+    export PT_HPU_SDPA_BR_FACTOR=4096       # slice size on the query
+    export PT_HPU_SDPA_BC_FACTOR=4096       # siice size on the kv
+    export VLLM_HPU_FSDPA_SLICE_CHUNK_SIZE=4096 # qkv slice size in fp8 FSDPA
+    export VLLM_HPU_FSDPA_SLICE_IMPL="slice_qkv"    # select the fp8 fsdpa impl
+    export VLLM_HPU_FSDPA_SLICE_CAUSAL="true"
+
+    # Enable MoE slice to reduce memory footprint
+    export VLLM_SUPPORT_MOE_SLICE=True
+    export VLLM_MOE_SLICE_LENGTH=8192
     clean_inc_scale
 else
     export VLLM_MOE_N_SLICE=8
